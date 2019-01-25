3d060eeb0d766fc19aabd22290809c92f151cdea
Claire TOFFANO-NIOCHE <claire.toffano-nioche@i2bc.paris-saclay.fr>
2019-01-25 18:40:50 +0100

    + accolades pr protection des variables

--- FAIR_script.sh
+++ FAIR_script.sh
@@ -3,14 +3,12 @@
 ## Author: T. Denecker & C. Toffano-Nioche
 ## Affiliation: I2BC
 ## Aim: A workflow to process RNA-Seq.
-## Organism : O. tauri
+## Organism: O. tauri
 ## Date: Jan 2019
 ## Step :
 ## 1- Create tree structure
 ## 2- Download data from SRA
-## 3- Run workflow (snakemake)
 ##------------------------------------------------------------------------------
-
 
 echo "=============================================================="
 echo "Creation of tree structure"
@@ -31,62 +29,37 @@ echo "=============================================================="
 echo "Download data from SRA"
 echo "=============================================================="
 
-# Get accession number (comment / uncomment to change methods)
- Accession="$(cut -f7 conditions.txt | tail -n +2)" # ascp method
-# Accession="$(cut -f1 conditions.txt | tail -n +2)" # fastq dump method
-# Accession="$(cut -f6 conditions.txt | tail -n +2)" # wget method
-
 cd Project/samples
 
 IFS=$'\n'       # make newlines the only separator
 for j in $(tail -n +2 ../../conditions.txt)
 do
-
-    access=$( echo "$j" |cut -f7 )
-    id=$( echo "$j" |cut -f1 )
+    
+    # Get important information from the line
+    access=$( echo "${j}" | cut -f6 )
+    id=$( echo "${j}" | cut -f1 )
+    md5=$( echo "${j}" | cut -f7 )
 
     echo "--------------------------------------------------------------"
     echo ${id}
     echo "--------------------------------------------------------------"
 
-    ascp -QT --file-checksum=md5 --file-manifest=text --file-manifest-path=. -l 300m -P33001 -i /home/miniconda3/etc/asperaweb_id_dsa.openssh ${access} .
+    # Download file
+    wget ${access} # wget method
 
-    md5_local="$(md5sum $id.fastq.gz | cut -d' ' -f1)"
-    echo $md5_local
-
-    if grep -q $md5_local *.txt
+    # Get md5 of downloaded file
+    md5_local="$(md5sum ${id}.fastq.gz | cut -d' ' -f1)"
+    echo ${md5_local}
+    
+    # Test md5 
+    if [ "${md5_local}" == "${md5}" ]
     then
         echo "Done"
     else
-        rm $id.fastq.gz
-        access=$( echo "$j" |cut -f6 )
-        wget ${access} # wget method
+        echo "Nope"
+        exit 1
     fi
-    rm *.txt
 done
 
 cd ../..
-
-echo "=============================================================="
-echo "Download annotations"
-echo "=============================================================="
-
-wget https://raw.githubusercontent.com/thomasdenecker/FAIR_Bioinfo/master/Data/O.tauri_annotation.gff -P Project/annotations
-
-echo "=============================================================="
-echo "Download genome"
-echo "=============================================================="
-
-wget https://raw.githubusercontent.com/thomasdenecker/FAIR_Bioinfo/master/Data/O.tauri_genome.fna -P Project/genome
-
-echo "=============================================================="
-echo "Snakemake"
-echo "=============================================================="
-
-snakemake
-
-echo "=============================================================="
-echo "Shiny app"
-echo "=============================================================="
-
-R -e "shiny::runApp('R-code', port=4444)"
+exit 0
